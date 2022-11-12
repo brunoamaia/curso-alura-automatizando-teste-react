ede0fa97421ef9fc96446b4cb36b3ed8cd3d5ba6
brunoamaia <brunoamaia94@gmail.com>
2022-11-11 21:02:03 -0300

    segundo modulo

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react'
-import App from './App'
+import App, { calcularNovoSaldo } from './App'
 
 describe('Componente principal', () => {
 	describe('Quando eu abro o App do banco', () => {
@@ -18,5 +18,39 @@ describe('Componente principal', () => {
 			render(<App />)
 			expect(screen.getByText('Realizar operação')).toBeInTheDocument()
 		})
+	})
+
+	describe('Quando eu realizo uma transação', () => {
+		describe('que é um saque, o valor vai', () => {
+			it('diminuir', () => {
+				const valores = {
+					transacao: 'saque',
+					valor: 50
+				}
+				const novoSaldo = calcularNovoSaldo(valores , 150);
+
+				expect(novoSaldo).toBe(100)
+			})
+			it('ficar negativo', () => {
+				const valores = {
+					transacao: 'saque',
+					valor: 100
+				}
+				const novoSaldo = calcularNovoSaldo(valores , 50);
+
+				expect(novoSaldo).toBe(-50)
+			})
+		})
+
+		it('que é um deposito, o valor vai aumentar', () => {
+			const valores = {
+				transacao: 'deposito',
+				valor: 50
+			}
+			const novoSaldo = calcularNovoSaldo(valores , 100);
+
+			expect(novoSaldo).toBe(150)
+		})
+	
 	})
 })
